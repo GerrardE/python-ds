58ee7b86fb6fcf61cb703d306947e699c3ad64f7
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-12-12 05:54:55 +0100

    Refactor int to binary

--- src/stack/int_to_bin.py
+++ src/stack/int_to_bin.py
@@ -1,17 +1,24 @@
 from stack import StackClass
 
 def convert_int_to_bin(dec_num):
+    if dec_num == 0:
+        return 0
+
     stack = StackClass()
     
     while dec_num > 0:
         stack.push(dec_num % 2)
         dec_num = dec_num // 2
 
-    bin = []
+    bin = ""
+    
     while not stack.is_empty():
-        bin.append(stack.pop())
+        bin = bin + str(stack.pop())
 
     return bin
 
 print("Num : 242 = 11110010")
 print(convert_int_to_bin(242))
+
+print("\nNum : 0 = 0")
+print(convert_int_to_bin(0))
